09f5c6d48a123331ea9e1630e0d587a6cc4dc70d
crosalot <crosalot@gmail.com>
2020-02-28 21:53:33 +0700

    add fields

--- schema/users.js
+++ schema/users.js
@@ -1,12 +1,16 @@
 export default {
   properties: {
     email: { type: 'keyword' },
+    password: { type: 'keyword' },
     name: { type: 'keyword' },
     avatarUrl: { type: 'keyword' },
 
     facebookId: { type: 'keyword' },
     githubId: { type: 'keyword' },
     twitterId: { type: 'keyword' },
+
+    belongTo: {type: 'keyword'},
+    isStaff: {type: 'boolean'},
 
     createdAt: { type: 'date' },
     updatedAt: { type: 'date' },
