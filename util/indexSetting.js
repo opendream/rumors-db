95f535ccc448839add3adfde7edbd341df4cb3ac
crosalot <crosalot@gmail.com>
2020-04-22 12:13:59 +0700

    fix synonym

--- util/indexSetting.js
+++ util/indexSetting.js
@@ -15,7 +15,7 @@ export default {
       analyzer: {
         cjk_url_email: {
           tokenizer: 'thai',
-          filter: ['cjk_width', 'lowercase', 'cjk_bigram', 'english_stop', 'stop', 'kstem', 'synonym'],
+          filter: ['lowercase', 'synonym', 'cjk_width', 'cjk_bigram', 'english_stop', 'stop', 'kstem'],
         },
       },
     },
